32934ba66abbf8897a4828b4fdf06a5693a9a216
Go-Marvin-at-289631370416 <liuyingwei0218@gmail.com>
2021-05-24 21:38:12 +0800

    update add chmod +x

--- install.sh
+++ install.sh
@@ -14,10 +14,10 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | echo y |sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | y |sudo tee /etc/apt/sources.list.d/docker.list > /dev/null -y
+  $(lsb_release -cs) stable" | echo y |sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
 
 ${COLOR}"Docker开始安装:"${END}
 sleep 2
