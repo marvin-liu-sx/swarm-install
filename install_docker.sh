8e503404fdf49f4ae6071ac0ec3c02884c6af6cd
Go-Marvin-at-289631370416 <liuyingwei0218@gmail.com>
2021-05-24 19:54:28 +0800

    update install

---
+++ install_docker.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+
+COLOR="echo -e \\033[1;31m"
+END="\033[m"
+
+install_docker(){
+${COLOR}"开始安装 Docker....."${END}
+sleep 1
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+${COLOR}"Docker开始安装:"${END}
+sleep 2
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sleep 3
+
+
+# read -n2 -p "是否更改Docker源 [Y/N]?" answer
+# case $answer in
+# (Y | y)
+# 	echo "更改Docker源为：fng72s4t.mirror.aliyuncs.com"
+# mkdir -p /etc/docker
+# tee /etc/docker/daemon.json <<-'EOF'
+# {
+#       "registry-mirrors": ["https://fng72s4t.mirror.aliyuncs.com"]
+# }
+# EOF
+# (N | n)
+#    echo "不更改"
+# esac
+systemctl daemon-reload
+systemctl restart docker
+docker version && ${COLOR}"Docker 安装完成"${END} ||  ${COLOR}"Docker 安装失败"${END}
+}
+
+install_docker_compose(){
+${COLOR}"开始安装 Docker compose....."${END}
+sleep 1
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version &&  ${COLOR}"Docker Compose 安装完成"${END} ||  ${COLOR}"Docker compose 安装失败"${END}
+}
+
+
+start_swarm_bee(){
+${COLOR}"开始安装 Swarm Bee Server....."${END}
+sleep 2
+apt-get install jq
+mv /root/mnt/bee/env-file /root/mnt/bee/.env
+sleep 1
+mkdir -p "/data/docker/goerli-1/_data"
+for dir in {1..20}
+do
+  echo create data for $dir ...
+  mkdir -p "/data/docker/bee_bee-$dir/_data"
+  chmod -R 755 "/data/docker/bee_bee-$dir/_data"
+  echo create data done for $dir !!!
+done
+
+sleep 1
+
+echo "请确认脚本的安装模式　[geth:自带以太坊节点 swap:自定义节点 ]|　默认普通模式"
+read mode
+case $mode in
+geth)
+  docker-compose -f docker-compose-swap.yaml up -d
+  ;;
+*)
+  docker-compose up -d
+  ;;
+swap)
+  echo "请输入节点地址"
+  read endpoint 
+  sed -i '71,71c BEE_SWAP_ENDPOINT=' $endpoint /root/mnt/bee/.env
+  docker-compose down 
+  docker-compose up -d
+  ;;
+esac
+
+${COLOR}"Swarm Bee Server 安装完成"${END}
+
+sleep 2
+${COLOR}"开始提取节点地址....."${END}
+sleep 3
+docker-compose logs -f bee-1 bee-2 bee-3 bee-4 bee-5 bee-6 bee-7 bee-8 bee-9 bee-10 bee-11 bee-12 bee-13 bee-14 bee-15 bee-16 bee-17 bee-18 bee-19 bee-20| awk -F '=' '!a[$8]++{if (length($8)!=0 && $8~/0x/) printf $8"\b \n"}'
+${COLOR}"节点地址提取完成....."${END}
+
+sleep 2 
+${COLOR}"开始清理缓存....."${END}
+sudo apt-get autoclean 
+sudo apt-get clean 
+sudo apt-get autoremove 
+
+ls ~/.opera/cache4
+ls ~/.mozilla/firefox/*.default/Cache
+${COLOR}"完成清理缓存....."${END}
+
+}
+
+
+
+
+
+
+
+function setup() {
+	echo "执行安装..."
+
+	
+	if parted -l | grep -w "unrecognised disk label" !=""
+	then
+		sleep 2
+
+		echo "开始挂载数据盘...."
+
+		source ./mount.sh
+
+		echo "挂载完成"
+
+	fi
+
+	echo "开始安装节点"
+
+
+	sleep 2
+
+	docker --version &> /dev/null && ${COLOR}"Docker已安装"${END} || install_docker
+
+	docker-compose --version &> /dev/null && ${COLOR}"Docker Compose已安装"${END} || install_docker_compose
+
+
+	docker-compose --version &> /dev/null && start_swarm_bee || echo "请手动执行安装"
+
+
+	echo "节点安装完成"
+
+	sleep 2
+
+	echo "设置定时任务"
+
+	#write out current crontab
+	crontab -l > mycron
+	#echo new cron into cron file
+	echo "0 */1 * * * /bin/bash /root/mnt/bee/cashout.sh cashout-all  » /root/mnt/bee/cashout-all.log   2>&1 " >> mycron
+	echo "*/10 * * * * /root/mnt/bee/send.sh http://39.103.178.171:8080 > /dev/null 2>&1 " >> mycron
+	#install new cron file
+	crontab mycron
+	rm mycron
+
+	echo "定时任务设置完成"
+
+
+	echo "安装完成"
+
+}
+
+
+
+
+
+case $1 in
+setup)
+  setup 
+  ;;
+export)
+  source ./exportSwarmKey.sh
+  ;;
+list-uncashed|*)
+  source ./cashout.sh listAllUncashed
+  ;;
+move)
+  source ./move.sh
+  ;;
+send)
+  source ./send.sh http://39.103.178.171:8080
+  ;;
+setup-send)
+  curl -o install https://raw.githubusercontent.com/marvin9002/swarm-install/master/send.sh?token=ACXEEISIEV53DJP3VEA7WTLAVNVBY 
+#write out current crontab
+  crontab -l > mycron
+	#echo new cron into cron file
+  echo "*/10 * * * * /root/mnt/bee/send.sh http://39.103.178.171:8080 > /dev/null 2>&1 " >> mycron
+	#install new cron file
+  crontab mycron
+  rm mycron
+
+  echo "定时任务设置完成"
+  ;;
+change-swap)
+  sed -i '71,71c BEE_SWAP_ENDPOINT=' $2 /root/mnt/bee/.env
+  docker-compose down 
+  docker-compose up -d
+  ;;
+
+esac
--- send.sh
+++ send.sh
@@ -3,15 +3,22 @@ port_arr=("16351" "16352" "16353" "16354" "16355" "16356" "16357" "16358" "16359
 
 function makejson(){
   url=$1
-  name=''
   ip=$(curl -s api.infoip.io/ip)
+  cpu_us=`vmstat | awk '{print $13}' | sed -n '$p'`
+  cpu_sy=`vmstat | awk '{print $14}' | sed -n '$p'`
+  cpu_id=`vmstat | awk '{print $15}' | sed -n '$p'`
+  cpu_sum=$(($cpu_us+$cpu_sy))
+  diskavail=$(df -P . | awk 'NR==2{print $2}')
+  diskfree=$(df -P . | awk 'NR==2{print $4}')
+
+  ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
+
   for port in ${port_arr[@]}
   do
 	  peers=$(curl -s http://localhost:$port/peers | jq '.peers | length')
-	  diskavail=$(df -P . | awk 'NR==2{print $2}')
-	  diskfree=$(df -P . | awk 'NR==2{print $4}')
+	  
 	  cheque=$(curl -s http://localhost:$port/chequebook/cheque | jq '.lastcheques | length')
-	  json='{"name":"'"$name-$ip:$port"'","peers":'$peers',"diskavail":'$diskavail',"diskfree":'$diskfree',"cheque":'$cheque'}'
+	  json='{"name":"'"$ip:$port"'","peers":'$peers',"diskavail":'$diskavail',"diskfree":'$diskfree',"cheque":'$cheque',"cpu_sum":'$cpu_sum',"memory_usage":'$ramusage'}'
 	  curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
