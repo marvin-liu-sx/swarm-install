3789edb1d559c91e23f5fc2d09707228f5b4d3a7
Go-Marvin-at-289631370416 <liuyingwei0218@gmail.com>
2021-05-26 15:13:46 +0800

    update export

--- exportSwarmKey.sh
+++ exportSwarmKey.sh
@@ -26,21 +26,14 @@ port_arr=("16351" "16352" "16353" "16354" "16355" "16356" "16357" "16358" "16359
 
 
 Install_Main() {
-	if [ -f key.json]; then
-		rm key.json
-	fi
-	wget exportSwarmKey https://github.com/grodstrike/bee-swarm/raw/main/exportSwarmKey
-	chmod +x exportSwarmKey
-	echo "输入node的密码:"
-	read  n
-	echo '私钥提取……'
 
 
 	for dir in {1..20}
 	do
 		mkdir -p "/data/bee-keys/bee_bee-$dir"
-		cp -rv "/var/lib/docker/volumes/bee_clef-$dir/_data/keystore/*" "/data/bee-celf/bee_bee-${dir}"
-		cp -rv "/var/lib/docker/volumes/bee_clef-$dir/_data/password" "/data/bee-celf/bee_bee-${dir}"
+		mkdir -p "/data/bee-bee_clef/bee_clef-$dir"
+		cp -rv "/var/lib/docker/volumes/bee_clef-$dir/_data/keystore/*" "/data/bee-bee_clef/bee_clef-${dir}"
+		cp -rv "/var/lib/docker/volumes/bee_clef-$dir/_data/password" "/data/bee-bee_clef/bee_clef-${dir}"
 		# ./exportSwarmKey "/data/bee-keys/bee_bee-$dir/ $n > key_tmp.json"
 		#rm "/data/bee-keys/bee_bee-$dir/swarm.key"
 		# sed 's/^[^{]*//' key_tmp.json > key.json
@@ -53,6 +46,7 @@ Install_Main() {
         sudo cp -rv "/data/docker/bee_bee-${dir}/_data/statestore" "/data/bee-keys/bee_bee-${dir}"
 
 	done
-	tar -zcvf clef.tar.gz /data/bee-keys
+	tar -zcvf clef.tar.gz /data/bee-celf
+	tar -zcvf keys.tar.gz /data/bee-keys
 }
 Install_Main
