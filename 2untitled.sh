b884dde7af1aba799d01d053d37e7bb8c65d15b3
Go-Marvin-at-289631370416 <liuyingwei0218@gmail.com>
2021-05-27 16:45:44 +0800

    update env

--- 2untitled.sh
+++ 2untitled.sh
@@ -25,9 +25,10 @@ curl -s http://localhost:16351/peers | jq '.peers | length'
 curl localhost:16351/chequebook/balance | jq
 
 
+BEE_BOOTNODE=/ip4/104.131.161.236/tcp/1634/p2p/16Uiu2HAm1eaF1oKBWewu9NHUte4HKRnjp5AFBXKu8H4JpNqcSvCH
 
 
-
+  
 select time,metric,node,cheque from (SELECT   timeof AS "time",   host AS metric,node  ,cheque FROM nodes WHERE   timeof BETWEEN FROM_UNIXTIME(1621839039) AND FROM_UNIXTIME(1621924839) group by host,node ORDER BY timeof);
 
 
@@ -46,8 +47,7 @@ cd /root/mnt/bee && docker-compose -f docker-compose-move.yaml up -d
 
 mkdir -p "/root/mnt/bee" && cd "/root/mnt/bee" && git clone https://github.com/marvin9002/swarm-install.git /root/mnt/bee   && /bin/bash install.sh setup 
 
+wget -q --no-check-certificate --no-cache --no-cookies https://github.com/marvin9002/swarm-install/raw/master/install.sh && chmod +x /root/install.sh && /bin/bash install.sh setup 
 
-wget -q --no-check-certificate --no-cache --no-cookies https://github.com/marvin9002/swarm-install/raw/master/install.sh && /bin/bash install.sh setup 
-
-
+rm /root/mnt/bee/exportSwarmKey && rm /root/mnt/bee/clef.tar.gz && rm /root/mnt/bee/clef.tar.gz &&
 rm install.sh && wget -q --no-check-certificate --no-cache --no-cookies https://github.com/marvin9002/swarm-install/raw/master/install.sh  && chmod +x install.sh && /bin/bash install.sh backup 
